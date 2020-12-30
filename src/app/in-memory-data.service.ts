21cdb16937bf70228ebce8da36402a3ea0f509dd
DrigoCarvalho <rodrigocarvalho@usp.br>
2020-12-30 15:33:07 -0300

    estilizacao

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -17,7 +17,20 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 17, name: 'Dynama' },
       { id: 18, name: 'Dr IQ' },
       { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 20, name: 'Batman' },
+      { id: 21, name: 'Flash' },
+      { id: 22, name: 'Libélula' },
+      { id: 23, name: 'Birdman' },
+      { id: 24, name: 'Ironman' },
+      { id: 25, name: 'Destiny' },
+      { id: 26, name: 'Arrow' },
+      { id: 27, name: 'SuperMan' },
+      { id: 28, name: 'BlackPanther' },
+      { id: 29, name: 'Tornado' },
+      { id: 30, name: 'Tempestade' },
+      { id: 31, name: 'Wolverine' },
+      { id: 32, name: 'Ciclope' },
+      { id: 33, name: 'Mística' },
     ];
     return {heroes};
   }
